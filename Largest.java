754931176f5c6ea284553075b88ec921da65a628
stella-Lee <41104669+stella-Lee@users.noreply.github.com>
2020-05-27 17:53:04 +0900

    first version

---
+++ Largest.java
@@ -0,0 +1,11 @@
+public class Largest {
+	public static int largest(int[] list) {
+		int index, max = Integer.MAX_VALUE;
+		for(index=0;index<list.length;index++) {
+			if(list[index]>max) {
+				max=list[index];
+			}
+		}
+		return max;
+	}
+}
